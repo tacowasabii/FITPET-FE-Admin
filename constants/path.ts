cf6bd5d6391138ca8c4432e9f0ec7455d0db45f3
tacowasabii <godseoril812@gmail.com>
2024-09-06 16:04:31 +0900

    feat: route path

--- constants/path.ts
+++ constants/path.ts
@@ -1,5 +1,10 @@
 const ROUTES = {
   HOME: "/",
+  QUOTE_INQUIRIES: "/quote-inquiries",
+  INSURANCE: "/insurance",
+  URL_MANAGEMENT: "/url-management",
+  INQUIRIES: "/inquiries",
+  PARTNERSHIP: "/partnership",
 };
 
 export default ROUTES;
